c7bf899b0a2deea58ba1a81e5d9bb5311a1fd411
Vitalii <viga125@gmail.com>
2022-07-25 16:45:43 +0300

    user id

--- backend/account/views/profile.py
+++ backend/account/views/profile.py
@@ -6,8 +6,12 @@ from account.models import UserProfile
 
 
 def profile(request):
-  
-    user_profile = UserProfile.objects.get(publicname = 'Vitalii')
-   
+    # user_id = request.user.useraccount
+    # user_profile = UserProfile.objects.get(publicname = 'Vitalii')
+    # print (user_id)
+    # user_profile = UserProfile.objects.get(id = user_id)
     
+    user_profile = request.user.userprofile
+    print(user_profile)
+
     return render(request,'account/profile.html',{"user_profile":user_profile });
