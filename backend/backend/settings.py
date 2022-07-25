594a3c322ff94aab545576b5007d67b0a9a6d8f0
Your Name <you@example.com>
2022-07-25 11:15:12 +0300

    commit from zdimon77@gmail.com ----

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,13 +117,6 @@ CSRF_TRUSTED_ORIGINS = ['https://*.zemlyak.in.ua','https://*.127.0.0.1','http://
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
