e08b071eddb2c0c92711f832a2b87dd3ab575bb0
Alex <alan.saf@ukr.net>
2022-08-14 11:13:46 +0300

    commit from Alex

--- backend/account/management/commands/load_cafe.py
+++ backend/account/management/commands/load_cafe.py
@@ -13,20 +13,42 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print('Loading cafe')
-        user_file = os.path.join(FIXTURES_PATH, 'cafe.json')
+        user_file = os.path.join(FIXTURES_PATH,'cafe', 'uzhorod.json')
+        #print(user_file)
         with open(user_file,'r') as f:
             jdata = json.loads(f.read())
-            Cafe.objects.all().delete()
-            for cafe in jdata:
-                city = City.objects.get(alias=cafe['city'])
-                c = Cafe()
-                c.name = cafe['name']
-                c.link = cafe['link']
-                c.desc_ru = cafe['desc_ru']
-                c.desc_en = cafe['desc_en']
-                c.desc_uk = cafe['desc_uk']
-                c.city = city
-                c.save()
-                c.set_groups()
-                print(cafe['name'])
+        
+
+        # cafe = Cafe()
+        # cafe.name = 'Alex'
+        # cafe.link = 'Odessa'
+        # cafe.desc_ru = 
+        # cafe.desc_en =
+        # cafe.desc_uk =
+        
+        # cafe.save()       
+    #print(jdata[1]["name"])
+        jdata = [{"name": "cafe1"}, {"name": "cafe2"}]
+        for cafe in jdata:
+            print(cafe["name"])
+            print(cafe["link"])
+            cafe = Cafe()
+            cafe.name = cafe["name"]
+            cafe.save()
+
+       
+
+        #     Cafe.objects.all().delete()
+        #     for cafe in jdata:
+        #         city = City.objects.get(alias=cafe['city'])
+        #         c = Cafe()
+        #         c.name = cafe['name']
+        #         c.link = cafe['link']
+        #         c.desc_ru = cafe['desc_ru']
+        #         c.desc_en = cafe['desc_en']
+        #         c.desc_uk = cafe['desc_uk']
+        #         c.city = city
+        #         c.save()
+        #         c.set_groups()
+        #         print(cafe['name'])
               
